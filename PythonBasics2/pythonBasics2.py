f8f502c61f3b66e83e15eeb007d2d7ce03eb9488
Hima Tulasi <htulasi@uncc.edu>
2021-09-27 23:03:53 -0400

    PythonBasics 2 - added logic to longest_conescutive_repeating_char function

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -23,7 +23,38 @@ def count_threes(n):
 def longest_consecutive_repeating_char(s):
   # YOUR CODE HERE
 
-  return
+  #declares a variable for stringLength and sets it equal to the length of string s
+  stringLength = len(s)
+  #initializes a count variable to be used to count the characters of the string
+  count = 1
+  #initializes a maximum variable used to stop the count of a string
+  #when a maximum amount of repeating letters is reached
+  maximum = 1
+  #initializes a variable as the first, or 0 in the array,
+  #character of the string s which is an array of characters
+  character = s[0]
+  #a for loop with the parameters of the first character in the string
+  #and the last character, denoted by the length of the string -1
+  for i in range(0, stringLength - 1):
+    #if statement to compare the current letter of the string to the next letter
+    if s[i] == s[i + 1]:
+      #if the condition above is met, the loop will continue counting the consecutive letters
+      count += 1
+    else:
+      #if the count of repeating characters exceeds the maximum number of repeating characters
+      #the maximum will be set to that count and the character will be set to the one where
+      #the repetition ends on the string or i.
+      #the count is then set to 1
+      if count > maximum:
+        maximum = count
+        character = s[i]
+      count = 1
+  #condition to account for if there is no repeating letters present so count can return to 0
+  #and the character can be reset to the last place in the array i
+  if count > maximum:
+    maximum = count
+    character = s[i]
+  return character
 # Part C. is_palindrome
 # Define a function is_palindrome(s) that takes a string s
 # and returns whether or not that string is a palindrome.
